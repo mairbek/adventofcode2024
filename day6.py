81224abfb2c7e93ca7183ae5f5e9e92fdb61730e
Mairbek Khadikov <mkhadikov@gmail.com>
2024-12-06 07:36:27 -0800

    brute force day 6

--- day6.py
+++ day6.py
@@ -33,8 +33,50 @@ def run_loop(i, j):
         i, j = ii, jj
     return result
 
+
+def check_loop(i, j, oi, oj):
+    visited = [['.' for _ in range(m)] for _ in range(n)]
+    dir = dirs.index(grid[i][j])
+    # print(dir, grid[i][j])
+    visited[i][j] = dirs[dir]
+    si, sj = i, j
+    while True:
+        ii, jj = i + dir_idx[dir][0], j + dir_idx[dir][1]
+        # print(ii, jj)
+        if ii < 0 or ii >= n or jj < 0 or jj >= m:
+            break
+        if grid[ii][jj] == '#':
+            dir = (dir + 1) % 4
+            continue
+        if visited[ii][jj] == dirs[dir]:
+            visited[si][sj] = 's'
+            visited[ii][jj] = 'o'
+            visited[oi][oj] = 'X'
+            # for row in visited:
+            #     print(row)
+            return True
+        visited[ii][jj] = dirs[dir]
+        i, j = ii, jj
+    return False
+
+si, sj = 0, 0
 for i in range(n):
     for j in range(m):
         if grid[i][j] in dirs:
-            print(run_loop(i, j))
+            si, sj = i, j
             break
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == '#':
+            continue
+        if i == si and j == sj:
+            continue
+        grid[i][j] = '#'
+        # print("---", i, j)
+        if check_loop(si, sj, i, j):
+            result += 1
+        grid[i][j] = '.'
+
+print(result)
