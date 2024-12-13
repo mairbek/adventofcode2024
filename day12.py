f3e1fa54815a55bf0fe6f55a77c7b14ffd4302a1
Mairbek Khadikov <mkhadikov@gmail.com>
2024-12-12 22:27:52 -0800

    solve day12

--- day12.py
+++ day12.py
@@ -1,4 +1,7 @@
+from email.policy import default
 import sys
+
+from collections import defaultdict
 
 grid = []
 for line in sys.stdin:
@@ -9,11 +12,22 @@ m = len(grid[0])
 
 visited = [[0 for _ in range(m)] for _ in range(n)]
 
+def count_seq(numbers):
+    sorted_numbers = sorted(numbers)
+    sequence_count = 0
+    previous_number = None
+    for num in sorted_numbers:
+        if previous_number is None or num != previous_number + 1:
+            sequence_count += 1
+        previous_number = num
+    return sequence_count
+
+
 def run_dfs(i, j):
     char = grid[i][j]
     area = 0
-    sides = 0
-    perimeter = 0
+    isides = defaultdict(set)
+    jsides = defaultdict(set)
     q = [(i, j)]
     while q:
         i, j = q.pop()
@@ -26,16 +40,25 @@ def run_dfs(i, j):
             if ii >= 0 and ii < n and jj >= 0 and jj < m and grid[ii][jj] == char:
                 q.append((ii, jj))
             else:
-                perimeter += 1
-    return (area, perimeter, sides)
+                if di == 0:
+                    jsides[(j, dj)].add(i)
+                elif dj == 0:
+                    isides[(i, di)].add(j)
+    total = 0
+    for key, value in isides.items():
+        count = count_seq(value)
+        total += count
+    for key, value in jsides.items():
+        count = count_seq(value)
+        total += count
+    return (area, total)
 
 total = 0
 for i in range(n):
     for j in range(m):
         if visited[i][j] == 1:
             continue
-        (area, perimeter, sides) = run_dfs(i, j)
-        print(grid[i][j], area, perimeter, area * perimeter)
+        (area, perimeter) = run_dfs(i, j)
         total += area * perimeter
 
 print(total)
