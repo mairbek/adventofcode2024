acba4573bf3ac1e70bfd04098f3e3a6c77d53b81
Mairbek Khadikov <mkhadikov@gmail.com>
2024-12-17 09:04:40 -0800

    solved day2

--- day16.py
+++ day16.py
@@ -13,18 +13,24 @@ for row in grid:
 def find_route(grid, si, sj):
     n = len(grid)
     m = len(grid[0])
-    visited = set()
+    costs = {}
+    min_score = None
     q = []
-    heapq.heappush(q, (0, si, sj, (0, 1)))
+    heapq.heappush(q, (0, 0, si, sj, (0, 1), [(si, sj)]))
+    items = set()
     while q:
-        score, i, j, dir = heapq.heappop(q)
-        if grid[i][j] == 'E':
-            return score
-        if (i, j) in visited:
-            continue
-        visited.add((i, j))
+        score, cost, i, j, dir, path = heapq.heappop(q)
         if grid[i][j] == '#':
             continue
+        pos = (i, j, dir)
+        if pos in costs and costs[pos] < score:
+            continue
+        costs[pos] = score
+        if grid[i][j] == 'E':
+            if not min_score or score == min_score:
+                min_score = score
+                for p in path:
+                    items.add(p)
         for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             ni, nj = i + di, j + dj
             if ni < 0 or ni >= n or nj < 0 or nj >= m:
@@ -35,23 +41,29 @@ def find_route(grid, si, sj):
                 cost += 1000
             if dot == -1:
                 continue
-            heapq.heappush(q, (score + cost, ni, nj, (di, dj)))
-    return None
-
+            heapq.heappush(q, (score + cost, cost, ni, nj, (di, dj), path + [(ni, nj)]))
+    return min_score, items
 
 n = len(grid)
 m = len(grid[0])
 start = None
+end = None
 for i in range(n):
     for j in range(m):
         if grid[i][j] == 'S':
             start = (i, j)
+        if grid[i][j] == 'E':
+            end = (i, j)
+        if start and end:
             break
-    if start:
+    if start and end:
        break
-print(start)
-if start:
+if start and end:
     si, sj = start
-    print(find_route(grid, si, sj))
-else:
-    print('No start point found')
+    ei, ej = end
+    score, items = find_route(grid, si, sj)
+    for i, j in items:
+        grid[i][j] = 'O'
+    for row in grid:
+        print(''.join(row))
+    print(score, len(items))
