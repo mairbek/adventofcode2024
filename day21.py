da188a259d18dc851279b6f9f79f7ea080b8c0a6
Mairbek Khadikov <mkhadikov@gmail.com>
2025-01-01 14:18:20 -0800

    finally sovled day 21 with dp

--- day21.py
+++ day21.py
@@ -1,17 +1,19 @@
+import heapq
 import sys
+from collections import deque
 
 pad_grid = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    ['.', '0', 'A'],
+    ["7", "8", "9"],
+    ["4", "5", "6"],
+    ["1", "2", "3"],
+    [".", "0", "A"],
 ]
 pad_n = len(pad_grid)
 pad_m = len(pad_grid[0])
 
 track_grid = [
-    ['.', '^', 'A'],
-    ['<', 'v', '>'],
+    [".", "^", "A"],
+    ["<", "v", ">"],
 ]
 track_n = len(track_grid)
 track_m = len(track_grid[0])
@@ -19,68 +21,70 @@ track_m = len(track_grid[0])
 
 def di_to_symbol(di, dj):
     if di == -1 and dj == 0:
-        return '^'
+        return "^"
     if di == 1 and dj == 0:
-        return 'v'
+        return "v"
     if di == 0 and dj == -1:
-        return '<'
+        return "<"
     if di == 0 and dj == 1:
-        return '>'
-    raise ValueError(f'Invalid di, dj: {di}, {dj}')
+        return ">"
+    raise ValueError(f"Invalid di, dj: {di}, {dj}")
 
-def compute_moves(grid, si, sj, ei, ej):
-    # Determine vertical moves
-    vert = "v" * (ei - si) if ei > si else "^" * (si - ei)
-    hor = ">" * (ej - sj) if ej > sj else "<" * (sj - ej)
-    if ej > sj and grid[ei][sj] != '.':
-        return vert + hor + 'A'
-    if grid[si][ej] != '.':
-        return hor + vert + 'A'
-    return vert + hor + 'A'
 
-track_moves = {}
-for si in range(track_n):
-    for sj in range(track_m):
-        if track_grid[si][sj] == '.':
+def compute_moves(grid, si, sj, ei, ej, score_fn):
+    n = len(grid)
+    m = len(grid[0])
+    q = deque()
+    q = []
+    heapq.heappush(q, (0, si, sj, ""))
+    while q:
+        cost, i, j, path = heapq.heappop(q)
+        if i == -1 and j == -1:
+            return cost, path
+        if grid[i][j] == ".":
             continue
-        for ei in range(track_n):
-            for ej in range(track_m):
-                if track_grid[ei][ej] == '.':
-                    continue
-                moves = compute_moves(track_grid, si, sj, ei, ej)
-                track_moves[(track_grid[si][sj], track_grid[ei][ej])] = moves
-
-pad_moves = {}
-for si in range(pad_n):
-    for sj in range(pad_m):
-        if pad_grid[si][sj] == '.':
+        if i == ei and j == ej:
+            # special 'A' command in the end
+            s = "A"
+            prev = path[-1] if path else None
+            score = score_fn(prev, s)
+            npath = path + s
+            heapq.heappush(q, (cost + score, -1, -1, npath))
             continue
-        for ei in range(pad_n):
-            for ej in range(pad_m):
-                if pad_grid[ei][ej] == '.':
-                    continue
-                moves = compute_moves(pad_grid, si, sj, ei, ej)
-                pad_moves[(pad_grid[si][sj], pad_grid[ei][ej])] = moves
+        for di, dj in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < n and 0 <= nj < m and grid[ni][nj] != ".":
+                s = di_to_symbol(di, dj)
+                prev = path[-1] if path else None
+                score = score_fn(prev, s)
+                npath = path + s
+                heapq.heappush(q, (cost + score, ni, nj, npath))
+    raise ValueError(f"No path from ({si}, {sj}) to ({ei}, {ej})")
 
-def expand(input, grid):
-    result = []
-    prev = 'A'
-    for char in input:
-        result += grid[(prev, char)]
-        prev = char
-    return ''.join(result)
 
-def expand_levels(input, max_level):
-    result = input
-    for level in range(max_level + 1):
-        grid = pad_moves if level == 0 else track_moves
-        result = expand(result, grid)
-        print(result)
-    return result
-
-def extract_number(input):
-    numstr = ''.join([i for i in input if i.isnumeric()])
-    return int(numstr)
+max_depth = 25
+track_moves = []
+for depth in range(max_depth):
+    track_moves.append({})
+    for si in range(track_n):
+        for sj in range(track_m):
+            if track_grid[si][sj] == ".":
+                continue
+            for ei in range(track_n):
+                for ej in range(track_m):
+                    if track_grid[ei][ej] == ".":
+                        continue
+                    expand_fn = (
+                        (lambda prev, s: 1)
+                        if depth == 0
+                        else lambda prev, s: track_moves[depth - 1][(prev or "A", s)]
+                    )
+                    pathlen, moves = compute_moves(
+                        track_grid, si, sj, ei, ej, expand_fn
+                    )
+                    track_moves[depth][
+                        (track_grid[si][sj], track_grid[ei][ej])
+                    ] = pathlen
 
 inputs = []
 for line in sys.stdin:
@@ -89,8 +93,33 @@ for line in sys.stdin:
         break
     inputs.append(ls)
 
-out = 0
+pad_moves = {}
+for si in range(pad_n):
+    for sj in range(pad_m):
+        if pad_grid[si][sj] == ".":
+            continue
+        for ei in range(pad_n):
+            for ej in range(pad_m):
+                if pad_grid[ei][ej] == ".":
+                    continue
+                expand_fn = lambda prev, s: track_moves[max_depth - 1][(prev or "A", s)]
+                pathlen, moves = compute_moves(pad_grid, si, sj, ei, ej, expand_fn)
+                pad_moves[(pad_grid[si][sj], pad_grid[ei][ej])] = pathlen
+
+
+def extract_number(input):
+    numstr = "".join([i for i in input if i.isnumeric()])
+    return int(numstr)
+
+
+total = 0
 for inp in inputs:
-    result = expand_levels(inp, 2)
-    out += extract_number(inp) * len(result)
-print(out)
+    prev = "A"
+    result = 0
+    for c in inp:
+        pathlen = pad_moves[(prev, c)]
+        result += pathlen
+        prev = c
+    total += result * extract_number(inp)
+
+print(total)
