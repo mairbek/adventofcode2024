f220b3b76db2b77e6ce3615c830b11ab2f9aa753
Mairbek Khadikov <mairbek@conduit.xyz>
2025-05-21 08:07:15 -0700

    solved day 11 with prefix sums

--- 2023/day11.py
+++ 2023/day11.py
@@ -1,74 +1,59 @@
 import sys
-from operator import abs
 
 grid = []
 for line in sys.stdin:
     grid.append([c for c in line[:-1]])
 
-def expand(grid):
+def prefix_sums(grid):
+    # scale = 2 <- part 1
+    scale = 1000000
     n = len(grid)
     m = len(grid[0])
-
-    empty_cols = []
+    cols = [0]
     for i in range(n):
         empty = True
         for j in range(m):
             if grid[i][j] != '.':
                 empty = False
                 break
+        cols.append(cols[-1] + 1)
         if empty:
-            empty_cols += [i]
-    empty_rows = []
+            cols[-1] += (scale - 1)
+    rows = [0]
     for j in range(m):
         empty = True
         for i in range(n):
             if grid[i][j] != '.':
                 empty = False
                 break
+
+        rows.append(rows[-1] + 1)
         if empty:
-            empty_rows += [j]
-    nn = n + len(empty_cols)
-    nm = m + len(empty_rows)
+            rows[-1] += (scale - 1)
+    return cols, rows
 
-    print(n, m, nn, nm)
-
-    result = [['.' for _ in range(nm)] for _ in range(nn)]
-    ii = 0
-    for i in range(n):
-        jj = 0
-        for j in range(m):
-            if grid[i][j] != '.':
-                result[ii][jj] = grid[i][j]
-            jj += 1
-            if j in empty_rows:
-                jj += 1
-        ii += 1
-        if i in empty_cols:
-            ii += 1
-
-    return result
-
-def manhattan(ai, aj, bi, bj):
-    return abs(ai - bi) + abs(aj - bj)
-
-g = expand(grid)
+cdist, rdist = prefix_sums(grid)
 
 pairs = []
-n = len(g)
-m = len(g[0])
+n = len(grid)
+m = len(grid[0])
 
 for i in range(n):
     for j in range(m):
-        if g[i][j] == '#':
+        if grid[i][j] == '#':
             pairs.append((i, j))
 
-# print(pairs)
 result = 0
 for ii in range(len(pairs)):
     for jj in range(ii + 1, len(pairs)):
         ai, aj = pairs[ii]
         bi, bj = pairs[jj]
-        d = manhattan(ai, aj, bi, bj)
-        # print(ii+1, jj+1, d, ai, aj, bi, bj)
+        if ai > bi:
+            bi, ai = ai, bi
+        if aj > bj:
+            bj, aj = aj, bj
+        di = cdist[bi] - cdist[ai]
+        dj = rdist[bj] - rdist[aj]
+        d = di + dj
         result += d
 print(result)
