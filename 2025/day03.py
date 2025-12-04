e6504b3863b7b6042123180b3b634c40bd8f020b
Mairbek Khadikov <mkhadikov@gmail.com>
2025-12-03 21:31:31 -0800

    2025 day03 part2

--- 2025/day03.py
+++ 2025/day03.py
@@ -5,19 +5,35 @@ def main():
     result = 0
 
     for line in sys.stdin:
-        # TODO: implement solution
         s = line.strip()
-        x = 0
+        st = []
         for ch in s:
             i = int(ch)
-            nx = (x % 10) * 10 + i
-            nnx = (x // 10) * 10 + i
-            # print("nx", nx)
-            x = max(nx, nnx, x)
+            st.append(i)
+            if len(st) <= 12:
+                continue
+            i = 0
+            while i < len(st) - 1:
+                if st[i] < st[i + 1]:
+                    break
+                i += 1
+            del st[i]
+        x = int("".join([str(i) for i in st]))
         result += x
 
     print(result)
 
+
+# 9, 7, 8, 4
+# [9]
+# 97
+# 97, 98, 78 delete min?
+#
+# 4, 7, 8, 9, 5, 7
+# 47
+# 47 8 -> 78
+# 78 9 -> 89
+# 89 5 -> 95
 
 if __name__ == "__main__":
     main()
