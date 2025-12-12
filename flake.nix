7ca631fb2d3abff859b8109ae1d4b39b23a6facb
Mairbek Khadikov <mairbek@conduit.xyz>
2025-12-11 16:36:14 -0800

    nixify

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Python env for Advent of Code";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+            python3Packages.z3-solver
+            python3Packages.pip
+            python3Packages.numpy
+          ];
+
+          shellHook = ''
+            echo "Python: $(python --version)"
+            echo "Z3: $(python -c 'import z3; print(z3.get_version_string())' 2>/dev/null || echo 'available')"
+          '';
+        };
+      }
+    );
+}
