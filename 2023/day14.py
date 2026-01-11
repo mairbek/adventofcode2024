0a84223488c68129b594b374456e9a9968f5117d
Mairbek Khadikov <mkhadikov@gmail.com>
2026-01-11 09:27:16 -0800

    day14 part2

--- 2023/day14.py
+++ 2023/day14.py
@@ -7,9 +7,84 @@ def parse_input():
         line = line.strip()
         if not line:
             continue
-
-        rows.append(line)
+        rows.append([c for c in line])
     return rows
+
+
+def rotate_north(grid):
+    n = len(grid)
+    m = len(grid[0])
+
+    for j in range(m):
+        slot = 0
+        for i in range(n):
+            if grid[i][j] == "O":
+                grid[slot][j], grid[i][j] = grid[i][j], grid[slot][j]
+                slot += 1
+            if grid[i][j] == "#":
+                slot = i + 1
+
+
+def rotate_south(grid):
+    n = len(grid)
+    m = len(grid[0])
+
+    for j in range(m):
+        slot = n - 1
+        for i in range(n - 1, -1, -1):
+            if grid[i][j] == "O":
+                # print(f"swap {(slot, i)} {(i, j)}")
+                grid[slot][j], grid[i][j] = grid[i][j], grid[slot][j]
+                slot -= 1
+            if grid[i][j] == "#":
+                slot = i - 1
+
+
+def rotate_west(grid):
+    n = len(grid)
+    m = len(grid[0])
+
+    for i in range(n):
+        slot = 0
+        for j in range(m):
+            if grid[i][j] == "O":
+                grid[i][slot], grid[i][j] = grid[i][j], grid[i][slot]
+                slot += 1
+            if grid[i][j] == "#":
+                slot = j + 1
+
+
+def rotate_east(grid):
+    n = len(grid)
+    m = len(grid[0])
+
+    for i in range(n):
+        slot = m - 1
+        for j in range(m - 1, -1, -1):
+            if grid[i][j] == "O":
+                grid[i][slot], grid[i][j] = grid[i][j], grid[i][slot]
+                slot -= 1
+            if grid[i][j] == "#":
+                slot = j - 1
+
+
+def rotate_cycle(grid):
+    rotate_north(grid)
+    rotate_west(grid)
+    rotate_south(grid)
+    rotate_east(grid)
+
+
+def score(grid):
+    n = len(grid)
+    m = len(grid[0])
+    result = 0
+    for j in range(m):
+        for i in range(n):
+            if grid[i][j] == "O":
+                score = m - i
+                result += score
+    return result
 
 
 def part1(grid):
@@ -31,11 +106,27 @@ def part1(grid):
     return result
 
 
-def part2(data):
+def part2(grid):
     """Solve part 2."""
-    pass
+    for row in grid:
+        print("".join(row))
+    states = {}
+    scores = []
+    target = 1000000000
+    max_iter = 10000
+    for i in range(max_iter):  # max
+        rotate_cycle(grid)
+        grid_str = "".join(["".join(row) for row in grid])
+        if grid_str in states:
+            ci = states[grid_str]
+            position_in_cycle = (target - i - 1) % (i - ci)
+            print("cycle detected", i, ci, position_in_cycle)
+            return scores[ci + position_in_cycle]
+        states[grid_str] = i
+        scores.append(score(grid))
+    return None
 
 
 if __name__ == "__main__":
     data = parse_input()
-    print(f"{part1(data)}")
+    print(f"{part2(data)}")
